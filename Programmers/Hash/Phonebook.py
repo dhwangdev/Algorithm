66b3d301db11c3711b233e4047ebadb92760424a
dhwangdev <98380987+dhwangdev@users.noreply.github.com>
2022-02-11 22:38:28 +0900

    Create Phonebook.py

---
+++ Programmers/Hash/Phonebook.py
@@ -0,0 +1,13 @@
+"""
+https://programmers.co.kr/learn/courses/30/lessons/42577
+
+전화번호 목록
+"""
+
+def solution(phone_book):
+    phone_book = sorted(phone_book)
+
+    for p1, p2 in zip(phone_book, phone_book[1:]):
+        if p2.startswith(p1):
+            return False
+    return True
