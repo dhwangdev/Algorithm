3f8796e282539293b42144f492f21fffdff235bf
dhwangdev <98380987+dhwangdev@users.noreply.github.com>
2022-02-23 20:48:33 +0900

    Update Syntax.py

--- Python/Syntax.py
+++ Python/Syntax.py
@@ -14,6 +14,13 @@ enumerate(iterable, start)
 -adds a counter as the key of the enumerate object.
 -start: starts counting from this number. (0 is default)
 
-zip()
+zip(iterables1, iterables2, ...)
+-returns a zip object as an iterator of tuples
+-each item in each iterable is paired together
+-the iterable with the least items decides the length of the new iterable
+
+to unzip:
+a, b, c = zip(*zipped_iterable_name)
+
 
 """
