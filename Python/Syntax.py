cd96885ba34b48e50951497ae4005a1dc7de1e77
dhwangdev <98380987+dhwangdev@users.noreply.github.com>
2022-03-30 15:56:52 +0900

    Update Syntax.py

--- Python/Syntax.py
+++ Python/Syntax.py
@@ -36,7 +36,7 @@ ex) r"\bain" or r"ain\b" (the "r" in the beginning is making sure that the strin
 \S returns a match where the string does not contain a whitespace character
 \w returns a match where the string contains any word characters (characters from a to Z, digits from 0-9, and the underscore _ character)
 \W returns a match where the string does not contain any word characters
-
+\Z returns a match if the specified characters are at the end of the string
 
 
 
