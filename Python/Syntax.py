40e3a3ffbee21eac412cd537ed01bd0e8d31706e
dhwangdev <98380987+dhwangdev@users.noreply.github.com>
2022-04-02 00:10:49 +0900

    Update Syntax.py

--- Python/Syntax.py
+++ Python/Syntax.py
@@ -41,6 +41,7 @@ ex) r"\bain" or r"ain\b" (the "r" in the beginning is making sure that the strin
 Sets
 [arn] returns a match where one of the specified characters (a, r, or n) are present
 [a-n] returns a match for any lower case character, alphabetically between a and n
+[^arn] returns a match for nay character except a, r, and n
 
 
 List
