c86e579f9a1046f26b23392cdbadfbb38b58e414
dhwangdev <98380987+dhwangdev@users.noreply.github.com>
2022-05-24 21:35:16 +0900

    Update and rename Syntax.py to Language_Setup.py

---
+++ Python/Language_Setup.py
@@ -0,0 +1,18 @@
+Python- VS Code
+
+1. download latest version of python
+2. download VS Code
+3. get python extension (contains pylance, jupyter)
+
+Kotlin- Intellij
+
+1. download Intellij
+2. download jdk (Intellij will give a prompt to download the kotlin plugin)
+
+setup: 
+1. jdk 17 (or latest jdk version)
+2. project 16/ junit 5
+3. gradle jdk 11 (17 not yet fully usable for gradle)
+
+For Spring use:
+use Maven
