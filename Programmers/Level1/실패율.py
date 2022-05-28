883aacafb0faf1239fed83d27c64002c631e4140
dhwangdev <98380987+dhwangdev@users.noreply.github.com>
2022-05-28 19:22:31 +0900

    Update 실패율.py

--- Programmers/Level1/실패율.py
+++ Programmers/Level1/실패율.py
@@ -1,16 +1,17 @@
 def solution(N, stages):
     failRate = []
-    
-    for stage in range(N):
-        for player in stages:
-            n = 0
-            d = 0
-            if player == stage:
-                n += 1
 
-            if player >= stage:
+    for level in range(1, N+1):
+        n = 0
+        d = 0
+
+        for player in stages:
+            if player == level:
+                n += 1
+            if player >= level:
                 d += 1
 
+        if n == 0 and d == 0:
+            d += 1
+        
         failRate.append(n/d)
-
-    
