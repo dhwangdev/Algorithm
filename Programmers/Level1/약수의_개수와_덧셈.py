fe776dbe8043b28a0ee988d2a86a6a485406b8d4
dhwangdev <98380987+dhwangdev@users.noreply.github.com>
2022-05-31 18:18:50 +0900

    Update 약수의_개수와_덧셈.py

--- Programmers/Level1/약수의_개수와_덧셈.py
+++ Programmers/Level1/약수의_개수와_덧셈.py
@@ -1,2 +1,14 @@
 def solution(left, right):
-  return ans
+    ans = 0
+    for end in range(left, right + 1):
+        count = 0
+        for num in range(1, end+1):
+            if end % num == 0:
+                count += 1
+
+        if count % 2 == 0:
+            ans += end
+        else:
+            ans -= end
+
+    return ans
