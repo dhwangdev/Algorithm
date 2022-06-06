51e8968506f4faa0fdbf3c8936b7362c6d179bb2
dhwangdev <98380987+dhwangdev@users.noreply.github.com>
2022-06-07 00:03:03 +0900

    Update Language_Setup.py

--- Python/Language_Setup.py
+++ Python/Language_Setup.py
@@ -1,15 +1,15 @@
 Python- VS Code
-
 1. download latest version of python
 2. download VS Code
 3. get python extension (contains pylance, jupyter)
 
 Kotlin- Intellij
-
 1. download Intellij
 2. download jdk (Intellij will give a prompt to download the kotlin plugin)
 
-setup: 
+Java- Intellij
+1. download JDK
+
 1. jdk 17 (or latest jdk version)
 2. project 16/ junit 5
 3. gradle jdk 11 (17 not yet fully usable for gradle)
@@ -17,7 +17,20 @@ setup:
 For Spring (Java) use:
 use Maven (for Java)
 
-Javascript- VS Code
+Java- Eclipse
+1. download JDK
 
+Eclipse
+1. new java project
+2. configure JRE -> Add -> Standard VM -> JRE home directory -> (JDK) -> Finish -> Apply
+3. Finish
+4. Do NOT create module
+
+Create new class (Project already selected)
+1. name: Main
+2. Check: public static void main(String[] args)
+3. Finish
+
+Javascript- VS Code
 1. download node js
 2. get live server extension
