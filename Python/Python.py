932d70425c53633111e7ec3125c7db091018782b
dhwangdev <98380987+dhwangdev@users.noreply.github.com>
2022-06-12 20:51:56 +0900

    Update Python.py

--- Python/Python.py
+++ Python/Python.py
@@ -26,3 +26,19 @@ list(데큐_리스트)
 Iterable = [내용, etc]
 
 변수 = "세퍼레이터".join(Iterable)
+
+# <List copy>
+"""
+- A new list assigned to an old list will incur problems when the new list is changed as the old list will change with it. 
+- to resolve this the copy method/ module must be used.
+"""
+
+#1. Shallow copy (can usually be used but not for nested lists)
+list1 = [1, 2, 3]
+list2 = list1.copy()
+
+#2. Deep copy (use this for nested lists)
+import copy
+list1 = [1, [2, 3], 4]
+list2 = copy.deepcopy(list1)
+
