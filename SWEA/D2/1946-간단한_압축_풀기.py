e3b28c4e98504534f1f880240597a41fe9bf8ef4
dhwangdev <98380987+dhwangdev@users.noreply.github.com>
2022-07-11 22:59:10 +0900

    Update 1946-간단한_압축_풀기.py

--- SWEA/D2/1946-간단한_압축_풀기.py
+++ SWEA/D2/1946-간단한_압축_풀기.py
@@ -3,19 +3,18 @@ T = int(input())
 main = []
 for ite1 in range(T):
     N = int(input())
-    temp = []
+    temp = ""
     for ite2 in range(N):
         fir, sec = input().split()
-        temp.append(fir*int(sec))
-        all = "".join(temp)
+        temp += fir*int(sec)
 
-        start = 0
-        end = 10
-        temp2 = []
-        for ite3 in range(len(all)//10 + 1):
-            temp2.append(all[start:end])
-            start += 10
-            end += 10
+    start = 0
+    end = 10
+    temp2 = []
+    for ite3 in range(len(temp)//10 + 1):
+        temp2.append(temp[start:end])
+        start += 10
+        end += 10
 
     main.append(temp2)
 
