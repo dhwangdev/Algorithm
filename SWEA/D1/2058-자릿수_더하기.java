a44d55d34017c532c45944ee0ffd04c78235c342
dhwangdev <98380987+dhwangdev@users.noreply.github.com>
2022-07-25 18:02:59 +0900

    Create 2058-자릿수_더하기.java

---
+++ SWEA/D1/2058-자릿수_더하기.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int N = input.nextInt();
+        int ans = 0;
+
+        while (N!=0) {
+            ans += N%10;
+            N /= 10;
+        }
+
+        System.out.println(ans);
+    }
+}
