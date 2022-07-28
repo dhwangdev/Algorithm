871a315d2e8469bb77cec7ac231c6c1b9a90cd77
dhwangdev <98380987+dhwangdev@users.noreply.github.com>
2022-07-28 14:16:30 +0900

    Create 2046-스탬프_찍기.java

---
+++ SWEA/D1/2046-스탬프_찍기.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Solution {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int num = sc.nextInt();
+		
+		for(int ite=0; ite<num; ite++) {
+			System.out.print("#");
+		}
+		sc.close();
+	}
+}
