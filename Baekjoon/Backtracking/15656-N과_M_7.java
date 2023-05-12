a5964f70bc619a8d5a93bfc8b96cfd9bd718882e
dhwangdev <98380987+dhwangdev@users.noreply.github.com>
2023-05-12 19:32:02 +0900

    Create 15656-N과_M_7.java

---
+++ Baekjoon/Backtracking/15656-N과_M_7.java
@@ -0,0 +1,45 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static int len, lim;
+    static int[] arr, output;
+    static StringBuilder sb = new StringBuilder();
+
+    static void dfs(int depth) {
+        if (depth==lim) {
+            for (int i=0; i<lim; i++) {
+                sb.append(output[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int idx=0; idx<len; idx++) {
+            output[depth] = arr[idx];
+            dfs(depth+1);
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        len = Integer.parseInt(st.nextToken());
+        lim = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        arr = new int[len];
+        for (int i=0; i<len; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        output = new int[lim];
+
+        Arrays.sort(arr);
+
+        dfs(0);
+        System.out.println(sb);
+
+        br.close();
+    }
+}
