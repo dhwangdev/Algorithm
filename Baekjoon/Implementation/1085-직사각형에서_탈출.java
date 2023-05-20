67449b8d5cfbd04ee5bf1cf40244e78a9953f4d6
dhwangdev <98380987+dhwangdev@users.noreply.github.com>
2023-05-20 17:04:36 +0900

    Create 1085-직사각형에서_탈출.java

---
+++ Baekjoon/Implementation/1085-직사각형에서_탈출.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		st = new StringTokenizer(br.readLine());
+		
+		int x = Integer.parseInt(st.nextToken());
+		int y = Integer.parseInt(st.nextToken());
+		int w = Integer.parseInt(st.nextToken());
+		int h = Integer.parseInt(st.nextToken());
+		
+		int min = Math.min(x, w-x);
+		min = Math.min(min, Math.min(y, h-y));
+	
+		System.out.println(min);
+		
+		br.close();
+	}
+}
