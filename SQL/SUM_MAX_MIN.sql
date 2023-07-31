f0fe8549241af0dc79ca8b483dcd3a35bf8e28d9
dhwangdev <98380987+dhwangdev@users.noreply.github.com>
2023-07-31 10:30:38 +0900

    Update SUM_MAX_MIN.sql

--- SQL/SUM_MAX_MIN.sql
+++ SQL/SUM_MAX_MIN.sql
@@ -26,3 +26,12 @@ https://programmers.co.kr/learn/courses/30/lessons/59408
 
 SELECT COUNT(DISTINCT NAME) FROM ANIMAL_INS
 WHERE NAME IS NOT NULL;
+
+/*
+가격이 제일 비싼 식품의 정보 출력하기
+*/
+
+SELECT * 
+FROM FOOD_PRODUCT 
+WHERE PRICE = 
+(SELECT MAX(PRICE) PRICE FROM FOOD_PRODUCT);
